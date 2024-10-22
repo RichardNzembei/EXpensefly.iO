ba8e4309f2ce3f25997da0a4fb9063eee725304c
RichardNzembei <richardsonreuben78@gmail.com>
2024-10-22 13:28:08 +0300

    app vue commit

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -6,23 +6,21 @@
 import { onMounted, watch } from 'vue';
 import { useStore } from '@/stores/useStore';
 import { useRouter } from 'vue-router';
-
 const store = useStore();
 const router = useRouter();
-
 onMounted(async () => {
   await store.initializeUser();
-  if (!store.user) {
+  if (!store.userId) {
     router.push('/login');
   } else {
-    await store.fetchUserData(store.user.id);
+
+    await store.fetchUserData(store.userId);
   }
 });
-
 watch(
-  () => store.user,
-  (newUser) => {
-    if (!newUser) {
+  () => store.userId,
+  (newUserId) => {
+    if (!newUserId) {
       router.push('/login');
     }
   }
