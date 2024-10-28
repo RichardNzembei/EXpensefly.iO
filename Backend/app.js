9582ca4430f3f7936ea81bd4d3d7ef3ca992a556
RichardNzembei <richardsonreuben78@gmail.com>
2024-10-28 21:43:40 +0300

    update

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,7 @@ const userRoutes = require('./routes/users');
 const expenseRoutes = require('./routes/expenses');
 const targetsRoutes = require('./routes/targets');
 const savingsRoutes = require('./routes/savings')
-const imageRoutes=require('./routes/profileImage')
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,7 +18,7 @@ app.use('/api', userRoutes);
 app.use('/api', expenseRoutes);
 app.use('/api', targetsRoutes);
 app.use('/api', savingsRoutes);
-app.use('/api/images',imageRoutes)
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
