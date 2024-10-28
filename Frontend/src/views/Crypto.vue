4917516a042bddecaf5297590f6abe11769e835f
RichardNzembei <richardsonreuben78@gmail.com>
2024-10-28 22:31:11 +0300

    crypto update

--- Frontend/src/views/Crypto.vue
+++ Frontend/src/views/Crypto.vue
@@ -1,23 +1,37 @@
-<script setup>
-import navigationBar from '@/components/navigationBar.vue';
-import { useRouter } from 'vue-router';
-import { ref } from 'vue';
-import router from '@/router';
-const isModalOpen=ref(true)
-const toDashBoard=()=>{
-    router.push('/dashboard')
-}
-</script>
 <template>
-    <navigationBar/>
-    <div v-if="isModalOpen" class="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-75">
-        <div class="bg-white rounded-lg p-6 max-w-sm mx-auto text-center">
-            <h2 class="text-xl font-semibold text-green-600">Under Development</h2>
-            <p class="text-gray-700 mt-2">This feature is currently being developed. Please check back later!</p>
-            <button @click="toDashBoard" class="mt-4 w-full py-2 bg-green-600 text-white font-semibold rounded-md shadow hover:bg-green-500 focus:ring focus:ring-green-200 transition duration-200 ease-in-out">
-                Back
-            </button>
+    <navigationBar />
+    <span>
+        <RouterLink to="/dashboard">
+            <img src="../assets/icons/thin-arrow.png" alt="" class="h-8 w-8 ml-5">
+        </RouterLink>
+    </span>
+    <div class="flex flex-col items-center  h-screen ">
+        <div class=" p-8 max-w-lg w-full">
+            <h2 class="text-green-600 text-xl font-bold mb-6 text-center">Cryptocurrency</h2>
+
+            <div class="mb-4 flex justify-center">
+                <img src="../assets/icons/cryptocurrencies.png" alt="Crypto Icon" class="w-16 h-16" />
+            </div>
+
+            <p class="text-gray-700 text-sm text-center mb-4">
+                Cryptocurrency is a digital or virtual currency that uses cryptography for security.
+                It operates on a technology called blockchain, which ensures transparency and security in transactions.
+            </p>
+            <p class="text-gray-700 text-sm text-center mb-4">
+                Cryptocurrencies have gained popularity due to their potential for high returns,
+                as well as their use in decentralized finance (DeFi), enabling users to lend, borrow, and trade without
+                intermediaries.
+            </p>
+            <p class="text-gray-700 text-sm text-center mb-4">
+                However, investing in cryptocurrency comes with risks, including market volatility and regulatory
+                challenges.
+                Understanding these factors is crucial for anyone looking to invest or use cryptocurrencies.
+            </p>
+
         </div>
     </div>
-    <h2>SSMEs</h2>
 </template>
+
+<script setup>
+import navigationBar from '@/components/navigationBar.vue';
+</script>
