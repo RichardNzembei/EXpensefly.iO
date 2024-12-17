c2ad1d11493e01d9986f562f2bc379a5de1c4919
RichardNzembei <richardsonreuben78@gmail.com>
2024-12-17 16:47:27 +0300

    update

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports={
+// tailwind.config.js
+export default {
   content: [
     "./src/**/*.{html,js,vue}",
+    "./public/index.html",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
